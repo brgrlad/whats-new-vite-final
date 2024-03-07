a43ad536b2d15628e7bf08f85edb0f53f647a3e0
Bruno Grecchi <b_grecchi@hotmail.com>
2024-03-07 14:26:51 +0000

    mongo DB set up - working!

--- server/index.jsx
+++ server/index.jsx
@@ -1,4 +1,5 @@
 // CONNECT TO LOCAL SERVER HERE
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,13 +7,60 @@ const port = 4004;
 
 app.use(require("express").json());
 app.use(require("express").urlencoded());
-app.use(cors()); // Enable CORS
+app.use(cors());
+
+const password = "FcD6k95rwUsJE66Q";
+
+// if you dont't add the database name to the string (news-app in this case) mongo will automatically create one named test
+const connectionString = `mongodb+srv://bgrecchi:${password}@cluster-one.uuhpyes.mongodb.net/news-app?retryWrites=true&w=majority&appName=cluster-one`;
+
+let connection = async () => {
+  try {
+    let res = await mongoose.connect(connectionString);
+    console.log("Connected to DB!");
+  } catch (e) {
+    console.log("Error connection to do DB");
+    console.log("error", e);
+  }
+};
+
+const usersSchema = new mongoose.Schema({
+  name: String,
+  age: String,
+});
+
+// first argument (users2) will be the name of the collection!!!!
+const Users = mongoose.model("Users3", usersSchema);
+
+let getUsers = async () => {
+  try {
+    const users = await Users.find();
+    console.log("users?? >>>>", users);
+  } catch (error) {
+    console.log("error fetching", error);
+  }
+};
 
 app.get("/random", (req, res) => {
   console.log(["request body:", req.body]);
   res.send("test from backend!");
 });
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+connection().then(async () => {
+  // const newUsers = new Users({
+  //   name: "no news app",
+  //   age: "30",
+  // });
+
+  // try {
+  //   const savedUser = await newUsers.save();
+  //   console.log("User saved successfully:", savedUser);
+  // } catch (error) {
+  //   console.log("Error saving user:", error);
+  // }
+  getUsers();
+
+  // app.listen(port, () => {
+  //   console.log(`connected to ${port}`);
+  // });
 });
---
+++ server/schemas/UserSchema.jsx
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bookmarks: { type: Array, required: false },
+});
+
+module.exports = mongoose.model("UserSchema", UserSchema);
